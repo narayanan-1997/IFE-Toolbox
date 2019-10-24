229e9d89db23708b075d46c73761f306cb13eac3
narayanan-1997 <narayanan.ramesh2011@gmail.com>
2019-10-24 17:09:16 +0530

    Doxygen comments

--- header/logger/compress_log.h
+++ header/logger/compress_log.h
@@ -1,4 +1,6 @@
 #include<string>
 
+/// compress_log is a header file which defines the
+/// prototype for the function compress_file in the main.
 
 void compress_file(const string & file_name);
--- header/logger/input_log.h
+++ header/logger/input_log.h
@@ -1,5 +1,7 @@
-
 #include<string>
+
+/// input_log is a header file which defines the prototype for 
+/// the function debug_log in debug_log.cpp   
 
 void debug_log(const string& message);
 
