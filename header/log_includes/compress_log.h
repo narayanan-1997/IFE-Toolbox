7f0639cbe500e8e2f051e339d775508b6c0e206a
narayanan-1997 <narayanan.ramesh2011@gmail.com>
2019-10-24 16:43:21 +0530

    removed namespace std

--- header/log_includes/compress_log.h
+++ header/log_includes/compress_log.h
@@ -1,4 +1,4 @@
 #include<string>
-using namespace std;
+
 
 void compress_file(const string & file_name);
--- header/log_includes/input_log.h
+++ header/log_includes/input_log.h
@@ -1,6 +1,5 @@
 
 #include<string>
-using namespace std;
 
 void debug_log(const string& message);
 
